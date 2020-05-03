f9c99c7ab45b55d3325b61621c6b095a83eaafcc
romain <romain.soumard@etu.univ-amu.fr>
2020-05-03 20:52:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Image weekly sorter
+
+A simple python script to sort images out into directories based on the day of the week.
---
+++ src/image_weekly_sorter.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import imghdr
+import datetime
+import calendar
+import argparse
+import configparser
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+from pathlib import Path, PureWindowsPath
+
+
+DEFAULT_CONFIG_FILE = Path('config.ini')
+DEFAULT_SECTION_NAME = 'Directories'
+
+
+def main():
+    config_content = parse_config(DEFAULT_CONFIG_FILE, DEFAULT_SECTION_NAME)
+
+    agenda = { day : subject for day, subject in config_content }
+    dirs = list(agenda.values())
+
+    create_directories(dirs)
+    sort_image(agenda)
+
+
+def parse_config(conf_file, section):
+    check_config_file(conf_file)
+    parser = configparser.ConfigParser()
+    parser.read(conf_file)
+    return parser.items(section)
+
+
+def check_config_file(conf_file):
+    if not conf_file.is_file():
+        print('Must specify a config file')
+        sys.exit(1)
+
+
+def create_directories(directories: list):
+    for directory in directories:
+        create_directory(directory)
+    
+
+def create_directory(directory):
+    if not Path(directory).is_dir():
+        Path(directory).mkdir()
+
+
+def sort_image(agenda):
+    for file in os.listdir():
+        if not Path(file).is_dir() and str(imghdr.what(file)) != 'None':
+            image = Image.open(file)
+            dayname = creation_dayname(image)
+            image.close()
+            move_in_dir(file, agenda, dayname)
+
+def creation_dayname(image):
+    date = parse_date(get_creation_date(image))
+    date = datetime.datetime.strptime(date,'%d %m %Y').weekday()
+    return calendar.day_name[date].lower()
+
+
+def parse_date(date):
+    return " ".join(date.split(" ")[0].split(":")[::-1])
+
+
+def get_creation_date(img: Image):
+    info = img._getexif()
+    for tag in info.keys():
+        if TAGS.get(tag, tag) == 'DateTimeOriginal':
+            return info[tag] 
+    return None
+
+
+def move_in_dir(imagename, agenda, dayname):
+    if Path(agenda[dayname]).is_dir():
+        os.rename(Path(imagename), Path(agenda[dayname]) /  Path(imagename))
+
+
+if __name__ == '__main__':
+    main()
+
